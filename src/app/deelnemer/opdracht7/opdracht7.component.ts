0f703697600713de4258a80a879835b7ef6e5063
jasperbosch <jasperbosch@chello.nl>
2020-07-04 17:15:51 +0200

    feat: puzzelstukjes en plaatsing gefixed

--- src/app/deelnemer/opdracht7/opdracht7.component.ts
+++ src/app/deelnemer/opdracht7/opdracht7.component.ts
@@ -14,6 +14,7 @@ export class Opdracht7Component implements OnInit, AfterViewInit {
 
   imgWidth = 800;
   imgHeight = 582;
+  marge = 20;
 
   rasters: paper.Raster[] = [];
 
@@ -40,30 +41,52 @@ export class Opdracht7Component implements OnInit, AfterViewInit {
     const rect = new paper.Rectangle(topLeft, rectSize);
     const path = new paper.Path.Rectangle(rect);
     path.fillColor = new paper.Color('#e9e9ff');
+    path.sendToBack();
 
-    this.rasters[0].position = new paper.Point(200, 150);
-    this.rasters[1].position = new paper.Point(200, 500);
-    this.rasters[2].position = new paper.Point(200, 900);
-    this.rasters[3].position = new paper.Point(500, 200);
-    this.rasters[4].position = new paper.Point(500, 500);
-    this.rasters[5].position = new paper.Point(500, 900);
-    this.rasters[6].position = new paper.Point(800, 250);
+    this.rasters[0].data = {x: 124 + topLeft.x, y: 140};
+    this.rasters[1].data = {x: 172 + topLeft.x, y: 427};
+    this.rasters[2].data = {x: 675 + topLeft.x, y: 466};
+    this.rasters[3].data = {x: 674 + topLeft.x, y: 179};
+    this.rasters[4].data = {x: 412 + topLeft.x, y: 125};
+    this.rasters[5].data = {x: 438 + topLeft.x, y: 470};
+    this.rasters[6].data = {x: 377 + topLeft.x, y: 288};
+
+    // tslint:disable-next-line:prefer-for-of
+    for (let x = 0; x < this.rasters.length; x++) {
+      this.rasters[x].position = new paper.Point(
+        Math.random() * ((paper.view.center.x - 100) * 2) + 100,
+        Math.random() * ((paper.view.center.y - 100) * 2) + 100
+      );
+      const rotation = Math.floor(Math.random() * 4) * 90;
+      this.rasters[x].rotate(rotation);
+    }
 
 
     paper.project.activeLayer.children[0].selected = true;
 
     const kids = paper.project.activeLayer.children;
     // tslint:disable-next-line:prefer-for-of
-    for (let x = 0; x < 7; x++) {
-      kids[x].onMouseDown = this.onMouseDown.bind(this);
-      kids[x].onDoubleClick = this.onDoubleClick.bind(this);
-      kids[x].onMouseDrag = this.onMouseDrag.bind(this);
+    for (let x = 0; x < kids.length; x++) {
+      if (kids[x] instanceof paper.Raster) {
+        kids[x].onMouseDown = this.onMouseDown.bind(this);
+        kids[x].onDoubleClick = this.onDoubleClick.bind(this);
+        kids[x].onMouseDrag = this.onMouseDrag.bind(this);
+        kids[x].onMouseUp = this.onMouseUp.bind(this);
+      }
     }
 
   }
 
+  onMouseUp(event): void {
+    if (Math.abs(event.target.rotation) === 0) {
+      if (Math.abs(event.point.x - event.target.data.x) < this.marge &&
+        Math.abs(event.point.y - event.target.data.y) < this.marge) {
+        event.target.position = new paper.Point(event.target.data.x, event.target.data.y);
+      }
+    }
+  }
+
   onMouseDrag(event): void {
-    console.log(event);
     event.target.position = event.point;
   }
 
