7af1b1e982bc9f523bdc511cba58cdd0eebb05b7
jasperbosch <jasperbosch@chello.nl>
2020-07-05 13:21:46 +0200

    fix: sizes and positions

--- src/app/deelnemer/opdracht4/opdracht4.component.ts
+++ src/app/deelnemer/opdracht4/opdracht4.component.ts
@@ -10,7 +10,6 @@ export class Opdracht4Component implements OnInit, AfterViewInit {
 
   @ViewChild('myCanvas') myCanvas: ElementRef;
 
-  dots: paper.PointText;
   cursor: paper.Path.Circle;
 
   flipOn = 260;
@@ -27,12 +26,15 @@ export class Opdracht4Component implements OnInit, AfterViewInit {
     paper.view.autoUpdate = true;
 
     const point = new paper.Point(710, 30);
-    this.dots = new paper.PointText(point);
-    this.dots.content = '. . . . . .';
-    this.dots.fontSize = 270;
-    this.dots.strokeColor = new paper.Color(0, 0, 0);
-    this.dots.strokeWidth = 5;
-    this.dots.fillColor = new paper.Color(256, 256, 256);
+    const dots = new paper.PointText(point);
+    dots.content = '. . . . . .';
+    dots.fontSize = 270;
+    dots.strokeColor = new paper.Color(0, 0, 0);
+    dots.strokeWidth = 5;
+    dots.fillColor = new paper.Color(256, 256, 256);
+    dots.point = new paper.Point(paper.view.center.x - (dots.bounds.width / 2), 30);
+    dots.sendToBack();
+
 
     const tekst = new paper.PointText(new paper.Point(600, 300));
     tekst.content = 'autisme';
@@ -40,6 +42,10 @@ export class Opdracht4Component implements OnInit, AfterViewInit {
     tekst.strokeColor = new paper.Color(0, 0, 0);
     tekst.strokeWidth = 5;
     tekst.fillColor = new paper.Color(256, 256, 256);
+    tekst.point = new paper.Point(paper.view.center.x - (tekst.bounds.width / 2), 300);
+    tekst.sendToBack();
+
+    this.flipOn = tekst.bounds.y + (tekst.bounds.height / 2);
 
     this.cursor = new paper.Path.Circle({
       center: new paper.Point(10, 10),
@@ -48,28 +54,31 @@ export class Opdracht4Component implements OnInit, AfterViewInit {
       radius: 3,
     });
 
-    tekst.onMouseDrag = this.onMouseDrag;
-    tekst.onMouseMove = this.onMouseMove;
-    tekst.onMouseDown = this.onMouseDown;
+    tekst.onMouseDrag = this.onMouseDrag.bind(this);
+    tekst.onMouseMove = this.onMouseMove.bind(this);
+    tekst.onMouseDown = this.onMouseDown.bind(this);
+
+    this.cursor.bringToFront();
   }
 
 
   onMouseMove(event: any): void {
-    const y = this.flipOn + (this.flipOn - event.y);
-    if (this.cursor) {
-      this.cursor.position = new paper.Point(event.x, y);
+    if (event.y) {
+      const y = this.flipOn + (this.flipOn - event.y);
+      if (this.cursor) {
+        this.cursor.position = new paper.Point(event.x, y);
+      }
     }
   }
 
   onMouseDown(event: any): void {
-    console.log('mousedown', event);
     this.myPath = new paper.Path();
     this.myPath.strokeColor = 'black';
     this.myPath.strokeWidth = 5;
   }
 
   onMouseDrag(event: any): void {
-    const y = 250 + (250 - event.point.y);
+    const y = this.flipOn + (this.flipOn - event.point.y) - 75; // Ik weet niet war deze 75 vandaan komt...
     this.myPath.add(event.point.x, y);
   }
 
