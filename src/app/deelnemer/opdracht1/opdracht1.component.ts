c02ccbc2a88fc9df8fce5b951ef1446507eb436f
jasperbosch <jasperbosch@chello.nl>
2020-07-06 16:36:26 +0200

    fix: badkamer kleiner gemaakt

--- src/app/deelnemer/opdracht1/opdracht1.component.ts
+++ src/app/deelnemer/opdracht1/opdracht1.component.ts
@@ -1,17 +1,18 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-opdracht1',
   templateUrl: './opdracht1.component.html',
   styleUrls: ['./opdracht1.component.scss']
 })
-export class Opdracht1Component implements OnInit {
+export class Opdracht1Component implements OnInit, AfterViewInit {
 
   prevEvent;
   baseX;
   baseY;
 
   @ViewChild('keyhole') keyhole: ElementRef;
+  @ViewChild('bathroom') bathroom: ElementRef;
 
   constructor() {
   }
@@ -19,28 +20,34 @@ export class Opdracht1Component implements OnInit {
   ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+    this.bathroom.nativeElement.style.display = 'block';
+    this.keyhole.nativeElement.style.width = this.bathroom.nativeElement.style.width;
+  }
+
+
   onMouseMove(event: MouseEvent): void {
     if (this.prevEvent) {
       if (this.prevEvent !== event) {
         // var x = parseInt(document.querySelector('.keyhole').style.backgroundPositionX);
         // x = x + event.screenX - prevEvent.screenX;
-        let x = event.x - this.baseX - 1200;
-        if (x < -1150) {
-          x = -1150;
+        let x = event.x - this.baseX - 1000;
+        if (x < -960) {
+          x = -960;
         }
-        if (x > -35) {
-          x = -35;
+        if (x > -230) {
+          x = -230;
         }
         this.keyhole.nativeElement.style.backgroundPositionX = x + 'px';
 
         // var y = parseInt(document.querySelector('.keyhole').style.backgroundPositionY);
         // y = y + event.screenY - prevEvent.screenY;
-        let y = event.y - this.baseY - 1200;
-        if (y < -1150) {
-          y = -1150;
+        let y = event.y - this.baseY - 1000;
+        if (y < -960) {
+          y = -960;
         }
-        if (y > -336) {
-          y = -336;
+        if (y > -430) {
+          y = -430;
         }
         this.keyhole.nativeElement.style.backgroundPositionY = y + 'px';
       }
